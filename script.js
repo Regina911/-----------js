1e4dc8dd0a1bee32451fe89350fe30e743818c18
Регина911 <shaybakovaregina08@gmail.com>
2025-02-03 15:54:22 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+//let var const
+//примитивные типы данных
+let num = 5 //number
+console.log(num);
+num = 6
+console.log(num);
+
+let bigNum = 222 //bigint
+ 
+const str = "Regina"  // string
+const str2 = "Regina2"
+console.log(str2);
+
+let married = true // boolen
+console.log(!!married);  //! - приведение к булевому значению
+
+let age = null  //null
+console.log(age);
+
+let sum = undefined //undefined
+console.log(sum);
+
+//let sym = Symbol{} //Symbol
+
+//Непримитивный тип данных Объект
+ let student = {
+    name: "Regina",  //пара ключ-значение
+    age: 31,
+    married: true,
+    adress: {
+        city: "Sterlitamak",
+        street: "Bluchera",
+        house: 25,
+    }
+ }
+ console.log(student.name);
+ //alert("Привет "+ student.name)
+ 
+ 
+
+
+
+
+
+
+
+
