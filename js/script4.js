43a2648ca2be78f728f6aa797d2b10afd41055aa
Регина911 <shaybakovaregina08@gmail.com>
2025-02-09 13:18:13 +0500

    lesson 4

---
+++ js/script4.js
@@ -0,0 +1,130 @@
+// Базовые операторы, математика
+
+//-2 - унарный             5*2 - бинарный оператор     5,2 - операнд
+
+//let x = 1
+
+// +  -  /  *  %  **
+//console.log( 7 % 2); 
+//console.log("Reg" + "ina");    //конкатенация  - сложение строк
+
+//console.log(`${x} Ramilevna`)
+
+//console.log(typeof (`1` + 2));
+
+//console.log(5 - `1`);                   = 4
+//console.log(`5` + `1`);                 = 51 (склеивает)
+//console.log(`5` * `2`);                 = 10
+
+//let x = 2
+//console.log(-x);                          = -2
+
+//let x = -2                                = 2
+//console.log(-x); 
+
+// Приведение к числу, унарный +
+//let x = true
+//console.log(Number(x));                 = 1
+
+// let x = true 
+// console.log( +x );    //1
+//  let a = false
+//  console.log( +a );   //0
+ 
+// let s = "10"
+// let d = "20"                      //= 1020
+// console.log(s + d);
+// console.log( +s  + + d);
+
+
+// Приоритет операторов
+
+// 1  - унарный (+, -)              +s -d
+// 2  - возведение в степень        **
+// 3  - умножение деление           * /
+// 4  - сложение вычитание          + -
+// 5 - присваивание                 =
+// let f  = 2 + 3 * 2**2
+// console.log(f);
+
+// let a = 1
+// let b = 2
+// let c = 3 - (a  = b + 1)
+// console.log(a);
+// console.log(c);
+// console.log(b);
+
+
+// //Присваивание по цепочке
+
+// let a, b, c
+
+// a = b = c = 52
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// Сокращенная арифметика с присваиванием
+
+// let a = 2
+// a = a + 5
+// a += 5
+
+// // Инкремент  ++    декремент --
+//  let num = 5                   // = 6
+//  //num++
+//  num = num + 1
+//  console.log(num);
+
+//  console.log(num++);  // постфиксная
+//  console.log(num);
+//  console.log(++num);  // префиксная
+//  console.log(num);
+ 
+//  let a = 2
+//  let b = a++
+//  console.log(b);
+
+// let a = 2
+// console.log(a++);  //2+1
+// ++a                //3+1  
+// console.log(a);    //= 4
+
+// let b = 4
+// console.log(2 * ++b);  
+// console.log(2 * --b);
+
+// Оператор , 
+//  let a = (1 + 2, 3 + 4)    // = 7 - сохраняет последнее значение
+//  console.log(a);
+
+//  null + 1
+//  console.log(null + 1);    = 1
+ 
+ 
+
+
+
+ 
+ 
+ 
+ 
+
+
+ 
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
