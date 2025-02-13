fe5f4f1c48f096cffa82e58b208dc3d7132293ae
Регина911 <shaybakovaregina08@gmail.com>
2025-02-13 12:46:45 +0500

    lesson4

---
+++ js/script5.js
@@ -0,0 +1,68 @@
+//Операторы сравнения
+
+// Числа
+// let a = 1
+// let b = 2
+// console.log( a < b );
+// console.log( a > b);
+// console.log( a <= b );
+// console.log( a >= b);
+// let result = a < b
+// console.log(result);
+
+// Строки
+// let a = 'А'
+// let b = 'Я'
+// console.log( a > b);
+
+// let a = 2
+// let b = 1
+// console.log( a == b );
+
+//можно сравнивать булевы значения с числом
+// let a  = true
+// let b = 1
+// console.log( a > b);
+
+// let a = 0
+// console.log(Boolean(a));   //false
+// let b = "0"
+// console.log(Boolean(b));   //true
+// console.log( a == b);      //true
+
+//Строгое равенство
+// let a = 0
+// let b = "O"
+// console.log( a === b);  //false
+
+// let a = undefined
+// let b = null
+// console.log( a == b);         //true       === - false
+
+// // null и 0
+// console.log(null>0);   //false
+// console.log(null<0);   //false
+// console.log(null>=0);  //true   <> работают неодинаково
+// console.log(null==0);    // сравнение преобразует null в число
+
+//undefined
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined<0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
