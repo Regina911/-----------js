26f183228ee4aff5a16481ff667a16d0ca16c204
Регина911 <shaybakovaregina08@gmail.com>
2025-02-13 19:28:17 +0500

    lesson5

---
+++ js/home.js
@@ -0,0 +1,20 @@
+// 5 > 4  //true
+// console.log(5 > 4 );     
+
+// "ананас" > "яблоко"     //true
+// console.log("ананас" > "яблоко");   //верно false
+
+// "2" > "12"    //false
+// console.log("2" > "12");           //верно false
+
+// undefined == null   //false
+// console.log(undefined == null);    //верно true
+
+// undefined === null  //false
+// console.log(undefined === null);   //верно false
+
+// null == "\n0\n"    не знаю
+// console.log(null == "\n0\n");      //верно false
+
+// null === +"\n0\n" не знаю
+// console.log(null === +"\n0\n");    //верно false
---
+++ js/script6.js
@@ -0,0 +1,70 @@
+//Условные операторы 'if', '?'
+
+// let a = prompt('Ваша зарплата?', 0)
+// console.log(typeof a);
+
+// if(a > 50000){
+//     alert ('Ok!!!')
+//  } else if(a >= 40000){
+//     alert('Нужен  2 поручителz')
+// } else if(a >= 30000){
+//     alert('Нужен 1  поручитель')
+//  }else{
+//     alert("Не одобрено")
+// }
+
+// Условный тернарный ? оператор
+
+// let a = prompt('Ваша зарплата?')
+
+//     console.log(a > 50 ? "Yes"  : "No")
+
+
+
+//1 task
+
+// if ("0") {
+//     alert ('Hello')
+// }
+
+// 2task
+// let a = prompt('Какое "официальное" название JavaScript?')
+// if (a ==="ECMAScript") {
+//     alert('Верно!')
+// }else {
+//     alert("Не знаете, ECMAScript")
+// }
+
+// 3 task
+// let a = prompt("Введите число")
+// if (a>0){
+//     alert(1)
+// }else if(a<0 ){
+//     alert(-1)
+// }else{
+//     alert (0)
+// }
+
+// 4 task
+// let result2 = (a + b < 4) ? "Мало": "Много"
+
+// 5 * task
+
+
+// if (login == 'Сотрудник') {
+//   message = 'Привет';
+// } else if (login == 'Директор') {
+//   message = 'Здравствуйте';
+// } else if (login == '') {
+//   message = 'Нет логина';
+// } else {
+//   message = '';
+// }
+ 
+// let message;
+
+// message = (login == 'Сотрудник') ? 'Привет' :
+//           (login == 'Директор') ? 'Здравствуйте':
+//           (login == '') ? 'Нет логина': ' '
+          
+
